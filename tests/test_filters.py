7b3bb46beecaf042027579763f60a85d0cb96b22
Christopher Graham <Chrisgraham908@gmail.com>
2022-07-31 15:07:40 +1200

    Created and tested new largest function

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
-from nodes.filters import smallest
+from nodes.filters import *
 
 
 class Test(TestCase):
     def test_smallest(self):
         self.assertEqual(smallest([1, 2]), 1)
+
+    def test_largest(self):
+        self.assertEqual(largest([1, 2]), 2)
